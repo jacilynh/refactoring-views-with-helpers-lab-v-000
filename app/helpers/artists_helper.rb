6ab8adaf1fa492417b1dfb5b214340cfcf333b1c
Jacilyn Hayden <jacihayden@gmail.com>
2018-07-28 19:58:32 +0000

    automatically backed up by learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,3 +1,9 @@
 module ArtistsHelper
-
+  def display_artist
+    if song.artist
+      link_to song.artist.name, song.artist
+    else
+      link_to "add artist", edit_song_path(song)
+    end
+  end
 end
